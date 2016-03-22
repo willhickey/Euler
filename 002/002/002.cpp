993233967ea47966704ddbcee70d7456506b3a4f
Will Hickey <csu_hickey@yahoo.com>
2016-03-21 21:54:03 -0600

    https://projecteuler.net/problem=2

---
+++ 002/002/002.cpp
@@ -0,0 +1,27 @@
+// 002.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	
+	long lastNumber = 1;
+	long currentNumber = 2;
+	long long sum = 0; 
+	while(currentNumber<4000000)
+	{
+		if(currentNumber%2==0)
+		{
+			sum+=currentNumber;
+		}
+		currentNumber+=lastNumber;
+		lastNumber = currentNumber-lastNumber;
+
+		printf("%d \n", currentNumber);
+	}
+	printf("The sum is: %d\n", sum);
+	return 0;
+}
+
